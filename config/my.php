65b0dd9656a91936a23a1d4804f267b02739e970
rabbit_dog <734787413@qq.com>
2016-11-15 09:42:28 +0800

    提交禅道源码

---
+++ config/my.php
@@ -0,0 +1,12 @@
+<?php
+$config->installed       = true;
+$config->debug           = true;//开发模式为true,生产模式为false
+$config->requestType     = 'PATH_INFO';
+$config->db->host        = '127.0.0.1';
+$config->db->port        = '3306';
+$config->db->name        = 'zentao';
+$config->db->user        = 'root';
+$config->db->password    = 'root';
+$config->db->prefix      = 'zt_';
+$config->webRoot         = getWebRoot();
+$config->default->lang   = 'zh-cn';
---
+++ tmp/log/sql.20161114.log.php
@@ -0,0 +1,8 @@
+<?php
+ die();
+?>
+20161114 18:30:12: misc-ping
+  SELECT * FROM `zt_config` WHERE owner IN ('system','admin') ORDER BY `id` 
+  SELECT * FROM `zt_lang` ORDER BY `lang`,`id` 
+  SELECT * FROM `zt_config` WHERE 1 = 1  AND  owner IN ('system') AND  module IN ('common') AND  section IN ('global') AND  `key` IN ('sn')
+
