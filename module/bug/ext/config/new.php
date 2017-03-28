5e6f6d2b3941185815db450a3965b6b3fe351144
RabbitDog1987 <734787413@qq.com>
2017-03-28 15:17:25 +0800

    暂停搜索条件变更开发

--- module/bug/ext/config/new.php
+++
@@ -1,4 +0,0 @@
-<?php
-$config->bug->search['params']['module']        = array('operator' => '=',  'control' => 'select', 'values' => 'modules');
-$config->bug->search['params']['activatedCount']= array('operator' => '=',      'control' => 'input',  'values' => '');
-$config->bug->search['params']['openedBuild']   = array('operator' => '=', 'control' => 'select', 'values' => 'builds');
