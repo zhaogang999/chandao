df832ed6e93395646db397e6b27bea43daac4301
RabbitDog1987 <734787413@qq.com>
2017-05-10 11:07:27 +0800

    禅道需求：1019。完善禅道任务搜索功能

--- module/project/ext/model/buildTaskSearchForm.php
+++ module/project/ext/model/buildTaskSearchForm.php
@@ -1,7 +1,4 @@
 <?php
-helper::import('H:\zentao\chandao\module\project\model.php');
-class extprojectModel extends projectModel 
-{
 /**
  * Build task search form.
  *
@@ -24,40 +21,4 @@ public function buildTaskSearchForm($projectID, $projects, $queryID, $actionURL)
     $this->config->project->search['params']['module']['values']  = $this->loadModel('tree')->getTaskOptionMenu($projectID, $startModuleID = 0);
 
     $this->loadModel('search')->setSearchParams($this->config->project->search);
-}
-/**
- * The control file of project module of ZenTaoPMS.
- *
- * @copyright   Copyright 2009-2012 青岛易软天创网络科技有限公司 (QingDao Nature Easy Soft Network Technology Co,LTD www.cnezsoft.com)
- * @license     business(商业软件) 
- * @author      Yangyang Shi <shiyangyang@cnezsoft.com>
- * @package     project 
- * @version     $Id$
- * @link        http://www.zentao.net
- */
-public function createRelationOfTasks($projectID)
-{
-    $this->loadExtension('gantt')->createRelationOfTasks($projectID);
-}
-
-public function editRelationOfTasks($projectID)
-{
-    $this->loadExtension('gantt')->editRelationOfTasks($projectID);
-}
-
-public function getRelationsOfTasks($projectID)
-{
-    return $this->loadExtension('gantt')->getRelationsOfTasks($projectID);
-}
-
-public function getDataForGantt($projectID, $type)
-{
-    return $this->loadExtension('gantt')->getDataForGantt($projectID, $type);
-}
-
-public function deleteRelation($id)
-{
-    $this->loadExtension('gantt')->deleteRelation($id);
-}
-//**//
 }
